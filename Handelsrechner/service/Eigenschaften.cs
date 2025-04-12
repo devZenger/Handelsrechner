ca570a4cc40a896626c1976c4f85e2cb662ab83c
devZenger <alwayscarryatowel@mail.de>
2025-04-12 09:27:19 +0200

    Klasse Eigenschaften erstellt

---
+++ Handelsrechner/service/Eigenschaften.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handelsrechner.service
+{
+    internal class Eigenschaften
+    {
+        public bool VersuchSetzen(object objekt, string eigenschaft, string wert)
+        {
+            try
+            {
+                var property = objekt.GetType().GetProperty(eigenschaft);
+                if (property != null && property.CanWrite)
+                {
+                    object value = Convert.ChangeType(wert, property.PropertyType);
+                    property.SetValue(objekt, value);
+                }
+                else
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+
+        public object lese (object objekt, string eigenschaft)
+        {
+            var property = objekt.GetType().GetProperty(eigenschaft);
+            if (property != null)
+            {
+                return property.GetValue(objekt);
+            }
+            else
+            {
+                throw new Exception($"Eigenschaft {eigenschaft} nicht gefunden.");
+            }
+        }
+
+
+    }
+}
